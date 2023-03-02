2b37165a80c33de9911b13e95e90d669e15d2acb
Andrey <Andre.68.1@mail.ru>
2023-03-03 00:21:45 +0500

    Pascal's triangle

---
+++ Homework8_004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+Console.Clear();
+Console.Write("Введите количество строк (от 10 до 15): ");
+    
+    int n = int.Parse(Console.ReadLine()!);
+        for (int y = 0; y < n; y++)
+        {
+            int c = 1;
+            for (int q = 0; q < n - y; q++)
+            {
+                Console.Write("   ");
+            }
+ 
+            for (int x = 0; x <= y; x++)
+            {
+                Console.Write("   {0:D} ", c);
+                c = c * (y - x) / (x + 1);
+            }
+        Console.WriteLine();
+        Console.WriteLine();
+        }
+Console.WriteLine();
